bfce6ff90e5d363b3eae157920f05fbb7172854a
agnishom <cagnishom@gmail.com>
2017-03-30 11:23:19 +0530

    Changed xrange to range (for Python 3); inserting a key returns pointer to the node; node now supports both key and value

--- fib-heap.py
+++ fib-heap.py
@@ -1,13 +1,15 @@
+
 class FibonacciHeap:
-    
-    # internal node class 
+
+    # internal node class
     class Node:
-        def __init__(self, data):
-            self.data = data
+        def __init__(self, key, value):
+            self.key = key
+            self.value = value
             self.parent = self.child = self.left = self.right = None
             self.degree = 0
             self.mark = False
-            
+
     # function to iterate through a doubly linked list
     def iterate(self, head):
         node = stop = head
@@ -19,17 +21,17 @@ class FibonacciHeap:
                 flag = True
             yield node
             node = node.right
-    
+
     # pointer to the head and minimum node in the root list
     root_list, min_node = None, None
-    
+
     # maintain total node count in full fibonacci heap
     total_nodes = 0
-    
+
     # return min node in O(1) time
     def find_min(self):
         return self.min_node
-         
+
     # extract (delete) the min node from the heap in O(log n) time
     # amortized cost analysis can be found here (http://bit.ly/1ow1Clm)
     def extract_min(self):
@@ -38,7 +40,7 @@ class FibonacciHeap:
             if z.child is not None:
                 # attach child nodes to root list
                 children = [x for x in self.iterate(z.child)]
-                for i in xrange(0, len(children)):
+                for i in range(0, len(children)):
                     self.merge_with_root_list(children[i])
                     children[i].parent = None
             self.remove_from_root_list(z)
@@ -50,28 +52,30 @@ class FibonacciHeap:
                 self.consolidate()
             self.total_nodes -= 1
         return z
-                    
+
     # insert new node into the unordered root list in O(1) time
-    def insert(self, data):
-        n = self.Node(data)
+    # returns the node so that it can be used for decrease_key later
+    def insert(self, key, value=None):
+        n = self.Node(key, value)
         n.left = n.right = n
         self.merge_with_root_list(n)
-        if self.min_node is None or n.data < self.min_node.data:
+        if self.min_node is None or n.key < self.min_node.key:
             self.min_node = n
         self.total_nodes += 1
-        
-    # modify the data of some node in the heap in O(1) time
+        return n
+
+    # modify the key of some node in the heap in O(1) time
     def decrease_key(self, x, k):
-        if k > x.data:
+        if k > x.key:
             return None
-        x.data = k
+        x.key = k
         y = x.parent
-        if y is not None and x.data < y.data:
+        if y is not None and x.key < y.key:
             self.cut(x, y)
             self.cascading_cut(y)
-        if x.data < self.min_node.data:
+        if x.key < self.min_node.key:
             self.min_node = x
-            
+
     # merge two fibonacci heaps in O(1) time by concatenating the root lists
     # the root of the new root list becomes equal to the first list and the second
     # list is simply appended to the end (then the proper min node is determined)
@@ -85,12 +89,12 @@ class FibonacciHeap:
         H.root_list.left = last
         H.root_list.left.right = H.root_list
         # update min node if needed
-        if h2.min_node.data < H.min_node.data:
+        if h2.min_node.key < H.min_node.key:
             H.min_node = h2.min_node
         # update total nodes
         H.total_nodes = self.total_nodes + h2.total_nodes
         return H
-        
+
     # if a child node becomes smaller than its parent node we
     # cut this child node off and bring it up to the root list
     def cut(self, x, y):
@@ -99,7 +103,7 @@ class FibonacciHeap:
         self.merge_with_root_list(x)
         x.parent = None
         x.mark = False
-    
+
     # cascading cut of parent node to obtain good time bounds
     def cascading_cut(self, y):
         z = y.parent
@@ -109,18 +113,18 @@ class FibonacciHeap:
             else:
                 self.cut(y, z)
                 self.cascading_cut(z)
-    
+
     # combine root nodes of equal degree to consolidate the heap
     # by creating a list of unordered binomial trees
     def consolidate(self):
         A = [None] * self.total_nodes
         nodes = [w for w in self.iterate(self.root_list)]
-        for w in xrange(0, len(nodes)):
+        for w in range(0, len(nodes)):
             x = nodes[w]
             d = x.degree
             while A[d] != None:
-                y = A[d] 
-                if x.data > y.data:
+                y = A[d]
+                if x.key > y.key:
                     temp = x
                     x, y = y, temp
                 self.heap_link(y, x)
@@ -128,13 +132,13 @@ class FibonacciHeap:
                 d += 1
             A[d] = x
         # find new min node - no need to reconstruct new root list below
-        # because root list was iteratively changing as we were moving 
+        # because root list was iteratively changing as we were moving
         # nodes around in the above loop
-        for i in xrange(0, len(A)):
+        for i in range(0, len(A)):
             if A[i] is not None:
-                if A[i].data < self.min_node.data:
+                if A[i].key < self.min_node.key:
                     self.min_node = A[i]
-        
+
     # actual linking of one node to another in the root list
     # while also updating the child linked list
     def heap_link(self, y, x):
@@ -144,8 +148,8 @@ class FibonacciHeap:
         x.degree += 1
         y.parent = x
         y.mark = False
-        
-    # merge a node with the doubly linked root list   
+
+    # merge a node with the doubly linked root list
     def merge_with_root_list(self, node):
         if self.root_list is None:
             self.root_list = node
@@ -154,7 +158,7 @@ class FibonacciHeap:
             node.left = self.root_list
             self.root_list.right.left = node
             self.root_list.right = node
-            
+
     # merge a node with the doubly linked child list of a root node
     def merge_with_child_list(self, parent, node):
         if parent.child is None:
@@ -164,14 +168,14 @@ class FibonacciHeap:
             node.left = parent.child
             parent.child.right.left = node
             parent.child.right = node
-            
+
     # remove a node from the doubly linked root list
     def remove_from_root_list(self, node):
         if node == self.root_list:
             self.root_list = node.right
         node.left.right = node.right
         node.right.left = node.left
-        
+
     # remove a node from the doubly linked child list
     def remove_from_child_list(self, parent, node):
         if parent.child == parent.child.right:
